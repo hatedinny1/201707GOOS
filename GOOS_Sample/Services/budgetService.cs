2d569daed09a194d450237fe4cfe86769dbb57a4
Powen <hatedinny1@gmail.com>
2017-07-01 16:09:10 +0800

    passed unit test two green lights , two red lights

--- GOOS_Sample/Services/budgetService.cs
+++ GOOS_Sample/Services/budgetService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using GOOS_Sample.Models;
 using GOOS_Sample.Models.DataModels;
 using GOOS_Sample.Models.ViewModels;
 
@@ -7,13 +8,22 @@ namespace GOOS_Sample.Services
 {
     public class BudgetService : IBudgetService
     {
+
+        private IRepository<Budget> _budgetRepository;
+
+        public BudgetService(IRepository<Budget> budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
         public void Create(BudgetAddViewModel model)
         {
             using (var dbContext = new NorthwindEntities())
             {
                 var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
-                dbContext.Budgets.Add(budget);
-                dbContext.SaveChanges();
+                //dbContext.Budgets.Add(budget);
+                //dbContext.SaveChanges();
+                this._budgetRepository.Save(budget);
             }
         }
     }
---
+++ GOOS_SampleTests/Services/BudgetServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using GOOS_Sample.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using GOOS_Sample.Models.DataModels;
+using GOOS_Sample.Models.ViewModels;
+using NSubstitute;
+using GOOS_Sample.Models;
+
+namespace GOOS_SampleTests.Services
+{
+    /// <summary>
+    /// BudgetServiceTests 的摘要說明
+    /// </summary>
+    [TestClass]
+    public class BudgetServiceTests
+    {
+        public BudgetServiceTests()
+        {
+            //
+            // TODO: 在此加入建構函式的程式碼
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///取得或設定提供目前測試回合
+        ///相關資訊與功能的測試內容。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 其他測試屬性
+        //
+        // 您可以使用下列其他屬性撰寫測試: 
+        //
+        // 執行該類別中第一項測試前，使用 ClassInitialize 執行程式碼
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在類別中的所有測試執行後，使用 ClassCleanup 執行程式碼
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在執行每一項測試之前，先使用 TestInitialize 執行程式碼 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在執行每一項測試之後，使用 TestCleanup 執行程式碼
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+
+        private BudgetService _budgetService;
+        private IRepository<Budget> _budgetRepositoryStub = Substitute.For<IRepository<Budget>>();
+        [TestMethod()]
+        public void CreateTest_should_invoke_repository_one_time()
+        {
+            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+            this._budgetService.Create(model);
+            _budgetRepositoryStub.Received()
+                .Save(Arg.Is<Budget>(x => x.Amount == 2000 && x.YearMonth == "2017-02"));
+        }
+
+    }
+}
