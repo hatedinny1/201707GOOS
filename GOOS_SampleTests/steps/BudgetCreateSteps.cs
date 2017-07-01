fa31238215e40929084a02be47afb5bc363cfa27
Powen <hatedinny1@gmail.com>
2017-07-01 16:36:35 +0800

    [Add] 更新預算 Steps

--- GOOS_SampleTests/steps/BudgetCreateSteps.cs
+++ GOOS_SampleTests/steps/BudgetCreateSteps.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using FluentAutomation;
+using GOOS_SampleTests.DataModelsForIntegrationTest;
 using TechTalk.SpecFlow;
 using GOOS_SampleTests.PageObjects;
+using TechTalk.SpecFlow.Assist;
 
 namespace GOOS_SampleTests.steps
 {
@@ -35,6 +37,17 @@ namespace GOOS_SampleTests.steps
         public void ThenItShouldDisplay(string message)
         {
             this._budgetCreatePage.ShouldDisplay(message);
+        }
+
+        [Given(@"Budget table existed budgets")]
+        public void GivenBudgetTableExistedBudgets(Table table)
+        {
+            var budgets = table.CreateSet<Budget>();
+            using (var dbcontext = new NorthwindEntitiesForTest())
+            {
+                dbcontext.Budgets.AddRange(budgets);
+                dbcontext.SaveChanges();
+            }
         }
     }
 }
