91694a46c8f79810b8779fc109f10de8710b33bc
Powen <hatedinny1@gmail.com>
2017-07-01 14:59:10 +0800

    refactor : green light

--- GOOS_SampleTests/steps/Common/Hooks.cs
+++ GOOS_SampleTests/steps/Common/Hooks.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FluentAutomation;
+using GOOS_Sample.Models.DataModels;
+using GOOS_SampleTests.DataModelsForIntegrationTest;
 using TechTalk.SpecFlow;
 
 namespace GOOS_SampleTests.steps.Common
@@ -17,6 +19,42 @@ namespace GOOS_SampleTests.steps.Common
         public static void SetBrowser()
         {
             SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+
+
+        [BeforeScenario()]
+        public void BeforeScenarioCleanTable()
+        {
+            CleanTableByTags();
+        }
+
+        [AfterFeature()]
+        public static void AfterFeatureCleanTable()
+        {
+            CleanTableByTags();
+        }
+
+        private static void CleanTableByTags()
+        {
+            var tags = ScenarioContext.Current.ScenarioInfo.Tags
+                .Where(x => x.StartsWith("Clean"))
+                .Select(x => x.Replace("Clean", ""));
+
+            if (!tags.Any())
+            {
+                return;
+            }
+
+            using (var dbcontext = new NorthwindEntities())
+            {
+                foreach (var tag in tags)
+                {
+                    dbcontext.Database.ExecuteSqlCommand($"TRUNCATE TABLE [{tag}]");
+                }
+
+                dbcontext.SaveChangesAsync();
+            }
         }
     }
 }
